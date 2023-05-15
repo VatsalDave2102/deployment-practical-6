c7424fcd0a93e38cffee17b2ab88b7f70d7374d5
Vatsal Dave <vatsal.d@simformsolutions.com>
2023-05-15 12:05:18 +0530

    Add staging build

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -25,11 +25,9 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-       THIS IS {(import.meta.env.MODE.toUpperCase())} MODE
-      </p>
+      <p className="read-the-docs">{import.meta.env.VITE_REACT_APP_BUILD}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
